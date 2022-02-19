5bc8be9513d1b8270ac275be20c7db8653e0035d
Alex Harston <alex@harston.io>
2022-02-19 23:24:24 +0000

    add formatting, CLI args and organisation filtering

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # github-pull
 
-A small script for pulling all of your GitHub repositories when setting a up a new machine, because I'm tired of doing it manually.
+## A small script for pulling all of your GitHub repositories when setting a up a new machine, because I'm tired of doing it manually.
 
 > **_Note:_** this is designed to pull the first 100 repos a user may have (100 being GitHub's limit before pagination). If you have over 100 repos, then a) you need to get your life together, and b) you can probably do this better than I can.
+  
+
+---
+
+
+_This is designed to work with Unix-based operating systems, and has not been tested on Windows._
+
+## Steps to run
+
+- Create a virtual environment with `virtualenv` (optional but highly recommended)
+- Generate a [GitHub personal access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token) for your GitHub account.
+- create an empty file called `.env` in the same folder as your code, and add the following key-value pairs:
+```
+GITHUB_PAT=yourpersonalaccesstoken123
+GITHUB_USERNAME=yourgithubusername
+```
+- Run `pip install -r requirements.txt` to install the required dependencies.
+- Run `python github-pull.py` if executing the file from the root folder you want to clone the repos to. Otherwise, run the file as a command-line tool, like follows:
+
+```python
+python github-pull.py --username alexharston --token abcdef123456 --output /home/alex/projects/ --include-orgs 
+```
+
+Note that adding a token or username via the command line will override the value stored in the `.env` file
--- github-pull.py
+++ github-pull.py
@@ -2,16 +2,43 @@ import kirjava
 import subprocess
 import os
 from dotenv import load_dotenv
+from tqdm import tqdm
+from rich import print
+from rich.padding import Padding
+import argparse
+
 
 load_dotenv()
 
-GITHUB_PERSONAL_ACCESS_TOKEN = os.getenv('GITHUB_PAT') # your GitHub Personal Access Token
-GITHUB_USERNAME = os.getenv('GITHUB_USERNAME')
+parser = argparse.ArgumentParser(description='Pull all your repositories from GitHub')
+
+parser.add_argument("-u", "--username", type=str, 
+                    help="The GitHub username to pull from.")
+parser.add_argument("-t", "--token", type=str, 
+                    help="The GitHub personal access token to use.")
+parser.add_argument("-o", "--output", type=str, 
+                    help="Output path to clone repositories into.")
+parser.add_argument("--include-orgs", dest='include_orgs', action='store_true', 
+                    help="Also pull repositories from organisations \
+                    you are a member of, if any exist. Otherwise just pull your personal repositories.")
+
+
+args = parser.parse_args()
+
+if args.token:
+    GITHUB_PERSONAL_ACCESS_TOKEN = args.token
+else:
+    GITHUB_PERSONAL_ACCESS_TOKEN = os.getenv('GITHUB_PAT')
+
+if args.username:
+    GITHUB_USERNAME = args.username
+else:
+    GITHUB_USERNAME = os.getenv('GITHUB_USERNAME')
 
 client = kirjava.Client(f"https://api.github.com/graphql")
 client.headers["Authorization"] = f"bearer {GITHUB_PERSONAL_ACCESS_TOKEN}"
 
-raw_response = client.execute(
+repos = client.execute(
     """
     query{
         viewer {
@@ -29,9 +56,37 @@ raw_response = client.execute(
     }
     """)
 
-repo_list = [n['nameWithOwner'] for n in raw_response['data']['viewer']['repositories']['nodes']]
 
-for r in repo_list:
-    if not os.path.exists(r.split("/")[1]):
-        if GITHUB_USERNAME in r:
-            subprocess.call(["git", "clone", f"git@github.com:{r}.git"])
+total_count = repos['data']['viewer']['repositories']['totalCount']
+print(f"You have {total_count} total repositories to pull")
+
+repo_list = [n['nameWithOwner'] for n in repos['data']['viewer']['repositories']['nodes']]
+
+
+if args.output:
+    directory = str(args.output)
+    print('Working directory is: ', directory)
+else:
+    directory = os.getcwd()
+    print('Working directory is: ', directory)
+
+print(type(args.username))
+
+if __name__ == "__main__":
+    print('Pulling repositories...')
+    for r in tqdm(repo_list):
+        if args.include_orgs:
+            if not os.path.exists(r.split("/")[1]):
+                test = Padding(f"[bold magenta]{r}[/bold magenta]", (2, 4))
+                print(test)
+                subprocess.call(["git", "clone", f"git@github.com:{r}.git"], cwd=directory)
+            else:
+                print(f"{r} already exists in this directory")
+        else:
+            if GITHUB_USERNAME in r:
+                if not os.path.exists(r.split("/")[1]):
+                    test = Padding(f"[bold magenta]{r}[/bold magenta]", (2, 4))
+                    print(test)
+                    subprocess.call(["git", "clone", f"git@github.com:{r}.git"], cwd=directory)
+                else:
+                    print(f"{r} already exists in this directory")
